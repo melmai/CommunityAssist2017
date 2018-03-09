46df9b27446952837397281591ca0e1914dbd5cd
Melissa Wong <melmai@DESKTOP-3JQGCE9.localdomain>
2018-03-08 16:49:44 -0800

    Application for Assistance

---
+++ CommunityAssist2017/Controllers/NewGrantController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CommunityAssist2017.Models;
+
+namespace CommunityAssist2017.Controllers
+{
+    public class NewGrantController : Controller
+    {
+        CommunityAssist2017Entities db = new CommunityAssist2017Entities();
+        // GET: NewGrant
+        public ActionResult Index()
+        {
+            if (Session["personKey"] == null)
+            {
+                Message m = new Message();
+                m.MessageText = "Must be logged in to apply!";
+                return RedirectToAction("Result", m);
+            }
+
+            ViewBag.GrantTypeKey = new SelectList(db.GrantTypes, "GrantTypeKey", "GrantTypeName");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Index([Bind(Include ="GrantTypeKey, GrantApplicationRequestAmount, GrantApplicationReason")] NewGrant ng)
+        {
+            GrantApplication grant = new GrantApplication();
+            grant.PersonKey = (int)Session["personKey"];
+            grant.GrantAppicationDate = DateTime.Now;
+            grant.GrantTypeKey = ng.GrantTypeKey;
+            grant.GrantApplicationRequestAmount = ng.GrantApplicationRequestAmount;
+            grant.GrantApplicationReason = ng.GrantApplicationReason;
+            db.GrantApplications.Add(grant);
+            db.SaveChanges();
+
+            Message m = new Message();
+            m.MessageText = "Thank you for applying! We will get back to you shortly.";
+
+            return View("Result", m);
+        }
+
+        public ActionResult Result(Message m)
+        {
+            return View(m);
+        }
+    }
+}
---
+++ CommunityAssist2017/Models/NewGrant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CommunityAssist2017.Models
+{
+    public class NewGrant
+    {
+        public int GrantTypeKey { get; set; }
+        public decimal GrantApplicationRequestAmount { get; set; }
+        public string GrantApplicationReason { get; set; }
+    }
+}
